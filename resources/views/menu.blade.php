e79594d843e82ae81b7f17b5d0d4bc22539d600e
Hi-Fiez <hifiez.soft@gmail.com>
2017-09-24 18:52:50 +0700

    navbar text-deco to none

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -3,11 +3,11 @@
         <div class="grid_12">
           <nav class="horizontal-nav full-width horizontalNav-notprocessed center">
             <ul class="sf-menu sf-js-enabled sf-arrows center">
-                <li class="current"><a href="" style="color: black; font-size: 15pt;">Home</a></li>
-                <li><a href="penyewaan" style="color: black; font-size: 15pt;">Toko Sewa</a></li>
-                <li><a href="about" style="color: black; font-size: 15pt;">Perusahaan</a></li>
-                <li><a href="login" style="color: black; font-size: 15pt;" class="right">Login</a></li>
-                <li><a href="daftar" style="color: black; font-size: 15pt;" class="right">Register</a></li>
+                <li class="current"><a href="" style="color: black; font-size: 15pt;text-decoration: none;">Home</a></li>
+                <li><a href="penyewaan" style="color: black; font-size: 15pt;text-decoration: none;">Toko Sewa</a></li>
+                <li><a href="about" style="color: black; font-size: 15pt;text-decoration: none;">Perusahaan</a></li>
+                <li><a href="login" style="color: black; font-size: 15pt;text-decoration: none;" class="right">Login</a></li>
+                <li><a href="daftar" style="color: black; font-size: 15pt;text-decoration: none;" class="right">Register</a></li>
             </ul>
           </nav>
           <div class="clear"></div>
